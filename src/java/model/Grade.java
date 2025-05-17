81d05ab70ce2a43d8bee8dc488ec07aa3c533a55
DuminduZ <dumindumadushanpremathilaka@gmail.com>
2025-05-17 18:05:22 +0530

    Create Grade.java
    Created a java file

---
+++ src/java/model/Grade.java
@@ -0,0 +1,23 @@
+public class Grade {
+    private int gradeId;
+    private int studentId;
+    private int subjectId;
+    private double marks;
+    private String grade;
+
+    // Getters and Setters
+    public int getGradeId() { return gradeId; }
+    public void setGradeId(int gradeId) { this.gradeId = gradeId; }
+
+    public int getStudentId() { return studentId; }
+    public void setStudentId(int studentId) { this.studentId = studentId; }
+
+    public int getSubjectId() { return subjectId; }
+    public void setSubjectId(int subjectId) { this.subjectId = subjectId; }
+
+    public double getMarks() { return marks; }
+    public void setMarks(double marks) { this.marks = marks; }
+
+    public String getGrade() { return grade; }
+    public void setGrade(String grade) { this.grade = grade; }
+}
