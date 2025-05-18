f19d13edbb3cdf1d137041b293e1c485507ec0c6
Nethmiperera22 <nnethmiperera4.6@gmail.com>
2025-05-18 13:56:48 +0530

    Create  Implement Message Module

--- src/java/controller/MessageServlet.java
+++ src/java/controller/MessageServlet.java
@@ -2,3 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package controller;
+
+/**
+ *
+ * @author nneth
+ */
+public class MessageServlet {
+    
+}
